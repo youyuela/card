76ab14ea2ffca341309a815f4e91faa507576ddc
youyuela <168611810+youyuela@users.noreply.github.com>
2024-07-26 19:26:01 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,9 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-    title: '文字生成图片工具',
-    description: '一个简单的文字生成图片工具',
+    title: '文字生成卡片工具',
+    description: '一款文字生成卡片在线工具',
+    keywords: '文字生成卡片,文字生成图片,文字生成大字报,文生图,文字转换图片,文字转换卡片,文字转换海报',
 }
 
 export default function RootLayout({
